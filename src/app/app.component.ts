187c2477c2debe450ca17e952804cbc00ce999ce
AdamPRankin <adamprankin@gmail.com>
2022-09-18 16:25:24 -0600

    add search city

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,15 +16,25 @@ export class AppComponent implements OnInit {
   cityName: string = 'Edmonton';
   weatherData!: WeatherData;
 
-  ngOnInit(): void {
-    this.weatherService.getWeatherData('Edmonton').subscribe({
-      next: (response) => {
-        this.weatherData = response;
-        console.log(response);
 
-      }
-    });
-    
+  ngOnInit(): void {
+  this.getWeatherData(this.cityName);
+  this.cityName = '';
   }
 
+  onSubmit() {
+  this.getWeatherData(this.cityName);
+  this.cityName = '';
+  }
+
+  private getWeatherData(cityName: string) {
+  this.weatherService.getWeatherData(cityName)
+  .subscribe({
+      next: (response) => {
+      this.weatherData = response;
+      console.log(response);
+      }
+  });
+  }
 }
+
